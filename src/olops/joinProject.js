cc7494cc720c9790a27f9acc52c73d9feed7195a
Xander Smeets <xander@smeets.ee>
2024-05-07 20:05:05 +0200

    Try something different

--- src/olops/joinProject.js
+++ src/olops/joinProject.js
@@ -63,14 +63,14 @@ module.exports =
 		// 	setTimeout( ( ) => resolve( undefined ), 1000 );
 		// } );
 		const promise = new Promise((resolve, reject) => {
-			socket.on('joinProjectResponse', (response) => {
-				resolve(response);
-			});
+			// socket.on('joinProjectResponse', (response) => {
+			// 	resolve(response);
+			// });
 
 			socket.emit(
 				'joinProject',
-				{ 'project_id': project_id }//,
-				//( self, res, owner, number ) => resolve( res )
+				{ 'project_id': project_id },
+				(res) => resolve( res )
 			);
 	
 			// setTimeout(() => {
